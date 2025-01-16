ec92362835921eb5ce23898b53acd3e693cb8d01
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-15 22:26:12 -0300

    Initial Commit

---
+++ src/main/java/com/uemg/servidor/farmalab/ServidorFarmaLab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uemg.servidor.farmalab;
+
+/**
+ *
+ * @author Caio Cezar Dias
+ */
+public class ServidorFarmaLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
