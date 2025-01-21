06e30ec2f496f26b4997323f7df880ee4ad65b19
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-20 22:17:47 -0300

    [Feature]: adicionado conteudo ao funcionario

--- src/main/java/model/Funcionario.java
+++ src/main/java/model/Funcionario.java
@@ -3,11 +3,68 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
+import java.io.Serializable;
 
 /**
  *
  * @author Caio Cezar Dias
  */
-public class Funcionario {
-    
+public class Funcionario extends Pessoa implements Serializable {
+    private String cargo;
+    private String password;
+    private float salario;
+    private boolean habilitado;
+
+    public Funcionario() {
+        super();
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + getId() +
+                ", nome='" + getNome() + '\'' +
+                ", cpf='" + getCpf() + '\'' +
+                ", endereco='" + getEndereco() + '\'' +
+                ", telefone='" + getTelefone() + '\'' +
+                ", created_at=" + getCreated_at() +
+                ", updated_at=" + getUpdated_at() +
+                ", cargo='" + cargo + '\'' +
+                ", password='[PROTECTED]'" +
+                ", salario=" + salario +
+                ", habilitado=" + habilitado +
+                '}';
+    }
 }
