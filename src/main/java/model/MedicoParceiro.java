95b5ceb427f42aef3f3b72750e580afe30cda0f0
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-20 22:50:18 -0300

    [Feature]: adicionado medico parceiro

---
+++ src/main/java/model/MedicoParceiro.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Caio Cezar Dias
+ */
+public class MedicoParceiro extends Pessoa implements Serializable {
+    private String crm;
+    private String estado;
+    private boolean habilitado;
+    private Timestamp created_at;
+    private Timestamp updated_at;
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+}
