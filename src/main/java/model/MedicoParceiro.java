2bf6c003a28bbb104bf81eb2d1ff064ad1bc5472
Isabely Melo <isabelymelo09@gmail.com>
2025-01-30 23:13:51 -0300

    [Ajuste]: construtor vazio MedicoParceiro

--- src/main/java/model/MedicoParceiro.java
+++ src/main/java/model/MedicoParceiro.java
@@ -17,7 +17,9 @@ public class MedicoParceiro extends Pessoa implements Serializable {
     private Timestamp created_at;
     private Timestamp updated_at;
 
-    
+    public MedicoParceiro() {
+        
+    }
     public MedicoParceiro(int id, String nome, String cpf, String endereco, String telefone, String crm, String estado, boolean habilitado, Timestamp created_at, Timestamp updated_at) {
         super(id, nome, cpf, endereco, telefone);
         setCrm(crm);
